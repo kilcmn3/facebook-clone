bc9521bff064010c1ee95ca7c2e4f05dca355093
kilcmn <kilcmn3@hotmail.com>
2020-10-14 22:36:17 -0400

    implementing nav bar

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Text, View } from 'react-native';
+import Navbar from './Navbar';
 
 export default function App() {
   return (
     <View>
+      <Navbar />
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style='auto' />
     </View>
--- Navbar.js
+++ Navbar.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
-import { Navigator, Text } from 'react-native';
+import { Text, Navigator, TouchableHighlight } from 'react-native';
 
-class Navbar extends Component {
+export default class Navbar extends Component {
   render() {
     return (
       <Navigator
         initialRoute={{ title: 'Awesome Scene', index: 0 }}
-        renderScene={(route, Navigator) => <Text> Hello {route.title}!</Text>}
+        renderScene={(route, navigator) => <Text>Hello {route.title}!</Text>}
         style={{ padding: 100 }}
       />
     );
   }
 }
-
-export default Navbar;
